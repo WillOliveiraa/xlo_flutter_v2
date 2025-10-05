f3ee9688b162d9d1ab303c175ec120fa5e5dafbe
Willian Oliveira <willian.oliveira@itsector.pt>
2025-10-05 11:37:18 +0100

    feat: add isUserLoggedIn, user

--- lib/src/features/auth/presentation/viewmodels/auth_viewmodel.dart
+++ lib/src/features/auth/presentation/viewmodels/auth_viewmodel.dart
@@ -6,6 +6,8 @@ class AuthViewModel {
   final GetCurrentUser _getCurrentUserUseCase;
   late final Command<void, User?> getCurrentUserCommand;
   late final Command<bool, bool> isUserLoggedIn;
+  User? _user;
+  bool _isUserLoggedIn = false;
 
   AuthViewModel(this._getCurrentUserUseCase) {
     getCurrentUserCommand = Command.createAsync(
@@ -19,12 +21,21 @@ class AuthViewModel {
     );
   }
 
+  void setIsUserLoggedIn(bool value) => _isUserLoggedIn = value;
+
+  bool get isUserLoggedInn => _isUserLoggedIn;
+
+  void setUser(User? value) => _user = value;
+
+  User? get user => _user;
+
   Future<User?> _getCurrentUser(_) async {
     final result = await _getCurrentUserUseCase();
     await Future.delayed(const Duration(seconds: 8));
-
     return result.fold((l) => throw l, (data) {
+      setUser(data);
       isUserLoggedIn.execute(true);
+      setIsUserLoggedIn(true);
       return data;
     });
   }
